96264e468082e40b05728ef271a8e8ed26c53c33
xyz <xyz@gmail.com>
2024-01-24 15:20:08 -0500

    commit added

---
+++ src/PartTimeStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JasKiran Kaur Sidhu
+ */
+public class PartTimeStudent extends Student {
+    
+    private int numOfCourses;
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+
+    public PartTimeStudent(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+}
